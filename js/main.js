6dfaeed2cfd9d855f098d565b6ab23e300b6e2bd
Marina <maribars93@yandex.ru>
2025-04-12 01:27:44 +0300

    feat: add generation of user photo descriptions and comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-{
-  id:
-  url:
-  description:
-  likes:
-  comments: [
-    {
-      id:
-        avatar:
-      message:
-        name:
-    }
-  ]
-}
-
 const PHOTO_DESCRIPTION_COUNT = 25;
-const photoDescriptions = ['Мгновение', 'Настроение', 'Деталь', 'Эмоции', 'История'];
+const photoDescriptions = [
+  'Мгновение',
+  'Настроение',
+  'Деталь',
+  'Эмоции',
+  'История'
+];
+
+const textMessages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const commentatorNames = [
+  'Иван',
+  'Алёна',
+  'Тимофей',
+  'Александр',
+  'Юлия'
+];
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -28,7 +36,7 @@ const createRandomNumberFromRangeGenerator = (min, max) => {
 
   return function () {
     if (previousValues.size >= (max - min + 1)) {
-      throw new Error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      throw new Error(`Перебраны все числа из диапазона от ${min} до ${max}`);
     }
     let currentValue = getRandomInteger(min, max);
     while (previousValues.has(currentValue)) {
@@ -41,21 +49,42 @@ const createRandomNumberFromRangeGenerator = (min, max) => {
 
 const generatePhotoId = createRandomNumberFromRangeGenerator(1, 25);
 const generatePhotoName = createRandomNumberFromRangeGenerator(1, 25);
-
 const createPhotoAddress = () => `photos/${generatePhotoName()}.jpg`;
 
-const createPhotoDescription = () => {
-  const randomPhotoDescriptionsIndex = getRandomInteger(0, photoDescriptions.length - 1);
+const createAvatarAddress = () => {
+  const generateAvatarName = getRandomInteger(1, 6);
 
-  return `${photoDescriptions[randomPhotoDescriptionsIndex]}`;
-}
+  return `img/avatar-${generateAvatarName}.svg`;
+};
 
+const getRandomArrayElement = (array) => {
+  const randomIndex = getRandomInteger(0, array.length - 1);
+
+  return `${array[randomIndex]}`;
+};
+
+//создание объекта с описанием комментария к фотографии пользователя
+const createUserPhotoComments = (index) => ({
+  id: index + 1,
+  avatar: createAvatarAddress(),
+  message: getRandomArrayElement(textMessages),
+  name: getRandomArrayElement(commentatorNames)
+});
+
+const generateUserPhotoCommentsCount = () => {
+  const userPhotoComments = Array.from({ length: getRandomInteger(0, 30) }, (_, i) => createUserPhotoComments(i));
+
+  return userPhotoComments;
+};
+
+//создание объекта с описанием фотографии, опубликованной пользователем
 const createUserPhotoDescription = () => ({
   id: generatePhotoId(),
   url: createPhotoAddress(),
-  description: createPhotoDescription()
+  description: getRandomArrayElement(photoDescriptions),
+  likes: getRandomInteger(15, 200),
+  comment: generateUserPhotoCommentsCount()
 });
-
 
 const userPhotosDescriptions = Array.from({ length: PHOTO_DESCRIPTION_COUNT }, createUserPhotoDescription);
 
