1f03c6cc11879f3228b5f2cca2686caf3e35eeb2
Marina <maribars93@yandex.ru>
2025-05-19 01:24:40 +0300

    Feat: Add photo modal opening logic on photo click

--- js/draw-users-photos.js
+++ js/draw-users-photos.js
@@ -1,16 +1,17 @@
-const photosContainer = document.querySelector('.pictures');
-const photoTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
-const usersPhotosListFragment = document.createDocumentFragment();
+import {
+  photoTemplate,
+  usersPhotosListFragment,
+  photosContainer
+} from './dom-elements.js';
 
 const drawUsersPhotos = (usersPhotos) => {
-  usersPhotos.forEach(({ url, description, likes, comments }) => {
+  usersPhotos.forEach(({ id, url, description, likes, comments }) => {
     const photoElement = photoTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = url;
     photoElement.querySelector('.picture__img').alt = description;
     photoElement.querySelector('.picture__comments').textContent = comments.length;
     photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.setAttribute('data-id', id);
     usersPhotosListFragment.appendChild(photoElement);
   });
   photosContainer.appendChild(usersPhotosListFragment);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { createPhotosDescriptions } from './data-generators/photos-descriptions.js';
 import { drawUsersPhotos } from './draw-users-photos.js';
+import { onPhotoClick } from './open-photo-modal.js';
 
 const usersPhotos = createPhotosDescriptions();
 
 drawUsersPhotos(usersPhotos);
+onPhotoClick(usersPhotos);
