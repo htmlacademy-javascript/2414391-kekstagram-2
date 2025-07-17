a83997f5b9e10251c00a64606ef0d038984fed9c
Yakubovskaya <maribars93@yandex.ru>
2025-07-17 15:53:05 +0300

    Feat: add photo-effects-slider

--- js/photo-upload-form.js
+++ js/photo-upload-form.js
@@ -57,7 +57,7 @@ const increaseScaleControlValue = () => {
     currentScaleValue = 100;
   }
 
-  scaleControlValue.value = `${currentScaleValue}% `;
+  scaleControlValue.value = `${currentScaleValue}%`;
   changePhotoScale(scaleControlValue);
 };
 
@@ -82,17 +82,36 @@ function initializePhotoScaleParams() {
   scaleControlSmallerButton.addEventListener('click', onScaleControlSmallerButtonClick);
 }
 
-//noUiSlider
+//slider
 
 noUiSlider.create(effectLevelSliderElement, {
-  start: 1,
+  start: 0,
   range: {
     'min': 0,
-    'max': 1
+    'max': 100
   },
-  step: 0.1,
-  connect: 'lower'
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
 });
+
+const updateSlider = (min, max, start, step) => {
+  effectLevelSliderElement.noUiSlider.updateOptions({
+    range: { min, max },
+    start,
+    step
+  });
+};
 
 const onEffectItemClick = (evt) => {
   const selectedEffect = evt.target.value;
@@ -103,39 +122,46 @@ const onEffectItemClick = (evt) => {
   }
   effectLevelContainer.style.display = 'block';
 
-  switch (selectedEffect) {
-    case 'sepia':
+  let updateFilter;
+  let startValue;
 
+  switch (selectedEffect) {
+    case 'chrome':
+      startValue = 1;
+      updateSlider(0, 1, startValue, 0.1);
+      updateFilter = (value) => `grayscale(${value})`;
+      break;
+    case 'sepia':
+      startValue = 1;
+      updateSlider(0, 1, startValue, 0.1);
+      updateFilter = (value) => `sepia(${value})`;
       break;
     case 'marvin':
-      effectLevelSliderElement.noUiSlider.updateOptions({
-        range: {
-          'min': 0,
-          'max': 100
-        },
-        start: 100,
-        step: 1
-      });
+      startValue = 100;
+      updateSlider(0, 100, startValue, 1);
+      updateFilter = (value) => `invert(${value}%)`;
       break;
     case 'phobos':
-      effectLevelSliderElement.noUiSlider.updateOptions({
-        range: {
-          'min': 0,
-          'max': 3
-        },
-        start: 3
-      });
+      startValue = 3;
+      updateSlider(0, 3, startValue, 0.1);
+      updateFilter = (value) => `blur(${value}px)`;
       break;
     case 'heat':
-      effectLevelSliderElement.noUiSlider.updateOptions({
-        range: {
-          'min': 1,
-          'max': 3
-        },
-        start: 3
-      });
+      startValue = 3;
+      updateSlider(1, 3, startValue, 0.1);
+      updateFilter = (value) => `brightness(${value})`;
       break;
   }
+
+  effectLevelSliderElement.noUiSlider.off('update');
+  effectLevelValueElement.value = startValue;
+  imgUploadPreview.style.filter = updateFilter(startValue);
+
+  effectLevelSliderElement.noUiSlider.on('update', () => {
+    const value = effectLevelSliderElement.noUiSlider.get();
+    effectLevelValueElement.value = value;
+    imgUploadPreview.style.filter = updateFilter(value);
+  });
 };
 
 function addPhotoEffect() {
